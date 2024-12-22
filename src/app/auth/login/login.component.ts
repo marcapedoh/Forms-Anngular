c81551eb1d1cf52deb4ea146940a5228842c5125
marcapedoh <adralabel@gmail.com>
2024-12-22 08:44:08 +0000

    reactive forms approach

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,41 +1,38 @@
 import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports:[ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [FormsModule]
 })
 export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef)
-  constructor() {
-    afterNextRender(() => {
-      const savedForm= JSON.parse(localStorage.getItem('saved-login-form')!)
-      //console.log(savedForm);
-      const savedEmail= savedForm.email
-      setTimeout(()=>{
-        this.form().controls['emailInput'].setValue(savedEmail)
-      },1)
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({ email: value.emailInput }))
-      })
 
-      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+  form=new FormGroup({
+    email: new FormControl('',{
+      validators:[Validators.email,Validators.required],
+    }),
+    password: new FormControl('',{
+      validators:[Validators.required,Validators.minLength(6)],
     })
+  });
+
+
+  get emailIsInvalid(){
+    return (
+      this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid
+    )
   }
-  onSubmit(form: NgForm) {
-    if (form.form.invalid) {
-      return;
-    }
-    const enteredEmail = form.form.value.emailInput;
-    const enteredPassword = form.form.value.passwordInput;
 
-
-    form.form.reset()
+  get passwordIsInvalid(){
+    return (
+      this.form.controls.password.touched && this.form.controls.password.dirty && this.form.controls.password.invalid
+    )
+  }
+  onSubmit(){
 
   }
 }
